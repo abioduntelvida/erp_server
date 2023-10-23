94a246a441022023013fee9550c1dcf79ac0d2a6
abioduntelvida <abiodun.olatunji@telvida.com>
2023-10-23 19:19:22 +0100

    fix base url

--- src/configs/auth.ts
+++ src/configs/auth.ts
@@ -1,6 +1,6 @@
 export default {
   meEndpoint: '/auth/me',
-  loginEndpoint: '172.19.2.11:4000/api/users/login',
+  loginEndpoint: 'http://172.19.2.11:4000/api/users/login',
   registerEndpoint: '/jwt/register',
   storageTokenKeyName: 'accessToken',
   storageDatekeyName: 'userData',
